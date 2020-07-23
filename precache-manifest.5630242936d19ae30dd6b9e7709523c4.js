78eeba42d31cbccf7d38bdb8ee49ae0d62fdbf06
BTC Lite Coin - BTCL <34189295+BTC-lite@users.noreply.github.com>
2020-07-23 18:08:32 +0530

    Add files via upload

---
+++ precache-manifest.5630242936d19ae30dd6b9e7709523c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaec314561d80d505eaff486a0d1d16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "525e279fee856f2ac8e9",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "95f2ae2dd73d8101194c",
+    "url": "./static/css/main.68c2ced0.chunk.css"
+  },
+  {
+    "revision": "525e279fee856f2ac8e9",
+    "url": "./static/js/2.bc22c9d7.chunk.js"
+  },
+  {
+    "revision": "8f85096b1f7efb5c4f364c96955ed4b7",
+    "url": "./static/js/2.bc22c9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f2ae2dd73d8101194c",
+    "url": "./static/js/main.65956768.chunk.js"
+  },
+  {
+    "revision": "63c9dbfb74b7006825a3",
+    "url": "./static/js/runtime-main.98c3ba2d.js"
+  }
+]);
